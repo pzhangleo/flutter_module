c54a3b9e5e613e895ebaae56109decdd24a343ae
pzhang <pengzh.leo@gmail.com>
2019-04-18 17:04:06 +0800

    增加下拉刷新

---
+++ m_flutter/lib/net/HttpManager.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+
+class HttpManager {
+  static var _dio;
+
+  static Dio getDio(String baseUrl) {
+    if (_dio == null) {
+      _dio = new Dio();
+      _dio.options.baseUrl = baseUrl;
+      _dio.options.connectTimeout = 6000;
+      _dio.options.receiveTimeout = 6000;
+    }
+    return _dio;
+  }
+}
--- m_flutter/lib/widget/ListViewDemo.dart
+++ m_flutter/lib/widget/ListViewDemo.dart
@@ -2,42 +2,54 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
-import 'package:http/http.dart' as http;
+import 'package:m_flutter/net/HttpManager.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class ListWidget extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => new _ListState();
-
 }
 
 class _ListState extends State<ListWidget> {
-
-  List data =[];
+  List data = [];
+  var _refreshController = new RefreshController();
 
   @override
   void initState() {
     super.initState();
-    loadData();
+    loadData(true);
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(title: Text("List Demo"), centerTitle: true,),
-      body: getListView(),
-    );
+        appBar: AppBar(
+          title: Text("List Demo"),
+          centerTitle: true,
+        ),
+        body: new SmartRefresher(
+            enablePullDown: true,
+            enablePullUp: true,
+            onRefresh: _onRefresh,
+            controller: _refreshController,
+            child: getListView()));
   }
 
-  void loadData() async {
+  void _onRefresh(bool up) {
+    loadData(up);
+  }
+
+  void loadData(bool up) async {
     try {
-      http.Response response = await http.get(
-          "https://jsonplaceholder.typicode.com/posts");
+      Response response = await HttpManager.getDio("")
+          .get("https://jsonplaceholder.typicode.com/posts");
+      print(response.data.toString());
+      _refreshController.sendBack(up, RefreshStatus.completed);
       setState(() {
-        data = json.decode(response.body);
+        data = response.data;
       });
-      print(response);
     } catch (e) {
+      _refreshController.sendBack(up, RefreshStatus.failed);
       print(e);
     }
   }
@@ -45,14 +57,14 @@ class _ListState extends State<ListWidget> {
   ListView getListView() {
     return ListView.builder(
       itemCount: data.length,
-      itemBuilder: (BuildContext context, int position){
+      itemBuilder: (BuildContext context, int position) {
         return getRow(position);
       },
     );
   }
 
   Widget getRow(int i) {
-    return Padding(padding: EdgeInsets.all(10.0), child: Text("Row ${data[i]["title"]}"));
+    return Padding(
+        padding: EdgeInsets.all(10.0), child: Text("Row ${data[i]["title"]}"));
   }
-
 }
